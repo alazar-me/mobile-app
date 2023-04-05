cd444173c83937b5053ecd7db81317f54ecfd47a
alazar <almaxvsbeko@gmail.com>
2023-04-05 15:20:16 +0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,8 @@ void main() {
           backgroundColor: Colors.grey[800],
         ),
         body: const Center(
-          child: Image(
-            image: NetworkImage(
-                'https://r4.wallpaperflare.com/wallpaper/195/656/894/space-stars-milky-way-wallpaper-87f6b111d730a7eddcecf072647b2704.jpg'),
-          ),
+          child:
+              Image(image: AssetImage('images/Diamond.png'), fit: BoxFit.cover),
         ),
       ),
     ),
